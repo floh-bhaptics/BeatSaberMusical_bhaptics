8d969e03959c24270b6dd1a2d95df0441797138d
floh-bhaptics <floh@fahrenbergers.de>
2022-02-18 11:00:23 +0100

    Projektdateien hinzufügen.

---
+++ MyBhapticsTactsuit.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Bhaptics.Tact;
+using bHapticsMusical;
+
+namespace MyBhapticsTactsuit
+{
+    public class TactsuitVR
+    {
+        /* A class that contains the basic functions for the bhaptics Tactsuit, like:
+         * - A Heartbeat function that can be turned on/off
+         * - A function to read in and register all .tact patterns in the bHaptics subfolder
+         * - A logging hook to output to the Melonloader log
+         * - 
+         * */
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        //private static ManualResetEvent HeartBeat_mrse = new ManualResetEvent(false);
+        // dictionary of all feedback patterns found in the bHaptics directory
+        public Dictionary<String, FileInfo> FeedbackMap = new Dictionary<String, FileInfo>();
+        public List<string> myEffectStrings = new List<string> { };
+
+#pragma warning disable CS0618 // remove warning that the C# library is deprecated
+        public HapticPlayer hapticPlayer;
+#pragma warning restore CS0618 
+
+
+        private static RotationOption defaultRotationOption = new RotationOption(0.0f, 0.0f);
+
+        public TactsuitVR()
+        {
+            LOG("Initializing suit");
+            try
+            {
+#pragma warning disable CS0618 // remove warning that the C# library is deprecated
+                hapticPlayer = new HapticPlayer("H3VR_bhaptics", "H3VR_bhaptics");
+#pragma warning restore CS0618
+                suitDisabled = false;
+            }
+            catch { LOG("Suit initialization failed!"); }
+            RegisterAllTactFiles();
+            LOG("Starting HeartBeat thread...");
+        }
+
+        public void LOG(string logStr)
+        {
+            Plugin.Log.Info(logStr);
+            //Plugin.Log.LogMessage(logStr);
+        }
+
+
+
+        void RegisterAllTactFiles()
+        {
+            // Get location of the compiled assembly and search through "bHaptics" directory and contained patterns
+            // string configPath = Directory.GetCurrentDirectory() + "\\Plugins\\bHaptics";
+            string configPath = IPA.Utilities.UnityGame.UserDataPath + "\\bHapticsPatterns";
+            //LOG("Path: " + configPath);
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.tact", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    hapticPlayer.RegisterTactFileStr(prefix, tactFileStr);
+                    LOG("Pattern registered: " + prefix);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+                FeedbackMap.Add(prefix, Files[i]);
+                if (prefix.StartsWith("LightEffect"))
+                {
+                    myEffectStrings.Add(prefix);
+                    LOG("Light effect pattern added: " + prefix);
+                }
+            }
+            systemInitialized = true;
+        }
+
+        public void PlaybackHaptics(String key, float intensity = 1.0f, float duration = 1.0f)
+        {
+            //LOG("Trying to play");
+            if (FeedbackMap.ContainsKey(key))
+            {
+                //LOG("ScaleOption");
+                ScaleOption scaleOption = new ScaleOption(intensity, duration);
+                //LOG("Submit");
+                hapticPlayer.SubmitRegistered(key, scaleOption);
+            }
+            else
+            {
+                LOG("Feedback not registered: " + key);
+            }
+        }
+
+        public void Recoil(string weaponName, bool isRightHand, float intensity = 1.0f)
+        {
+            // weaponName is a parameter that will go into the vest feedback pattern name
+            // isRightHand is just which side the feedback is on
+            // intensity should usually be between 0 and 1
+
+            float duration = 1.0f;
+            var scaleOption = new ScaleOption(intensity, duration);
+            // the function needs some rotation if you want to give the scale option as well
+            var rotationFront = new RotationOption(0f, 0f);
+            // make postfix according to parameter
+            string postfix = "_L";
+            if (isRightHand) { postfix = "_R"; }
+
+            // stitch together pattern names for Arm and Hand recoil
+            string keyHands = "RecoilHands" + postfix;
+            string keyArm = "Recoil" + postfix;
+            // vest pattern name contains the weapon name. This way, you can quickly switch
+            // between swords, pistols, shotguns, ... by just changing the shoulder feedback
+            // and scaling via the intensity for arms and hands
+            string keyVest = "Recoil" + weaponName + "Vest" + postfix;
+            hapticPlayer.SubmitRegistered(keyHands, scaleOption);
+            hapticPlayer.SubmitRegistered(keyArm, scaleOption);
+            hapticPlayer.SubmitRegistered(keyVest, scaleOption);
+        }
+
+
+        public bool IsPlaying(String effect)
+        {
+            return IsPlaying(effect);
+        }
+
+        public void PlaySpecialEffect(string effect)
+        {
+            foreach (string myEffect in myEffectStrings)
+            {
+                if (IsPlaying(myEffect)) return;
+            }
+            if (IsPlaying(effect)) return;
+            PlaybackHaptics(effect);
+        }
+
+        public void StopThreads()
+        {
+            // Yes, looks silly here, but if you have several threads like this, this is
+            // very useful when the player dies or starts a new level
+            //StopHeartBeat();
+        }
+
+
+    }
+}
---
+++ bHapticsMusicalController.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using UnityEngine;
+
+using HarmonyLib;
+using MyBhapticsTactsuit;
+
+namespace bHapticsMusical
+{
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class bHapticsMusical : MonoBehaviour
+    {
+        public static bHapticsMusical Instance { get; private set; }
+
+        public static TactsuitVR tactsuitVr;
+        public static List<string> myEffectStrings = new List<string> { };
+        public static Stopwatch timerLastEffect = new Stopwatch();
+        public static Stopwatch timerSameTime = new Stopwatch();
+        public static int numberOfEvents = 0;
+        public static int defaultTriggerNumber = 4;
+        public static int currentTriggerNumber = 4;
+        public static List<float> highWeights = new List<float> { };
+        public static float weightFactor = 1.0f;
+        public static bool reducedWeight = false;
+        public static bool ringEffectOff = false;
+        public static System.Random rnd = new System.Random();
+
+
+        // These methods are automatically called by Unity, you should remove any you aren't using.
+        #region Monobehaviour Messages
+        /// <summary>
+        /// Only ever called once, mainly used to initialize variables.
+        /// </summary>
+        private void Awake()
+        {
+            // For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
+            //   and destroy any that are created while one already exists.
+            if (Instance != null)
+            {
+                Plugin.Log?.Warn($"Instance of {GetType().Name} already exists, destroying.");
+                GameObject.DestroyImmediate(this);
+                return;
+            }
+            GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
+            Instance = this;
+            Plugin.Log?.Debug($"{name}: Awake()");
+            var harmony = new Harmony("bhaptics.musical.patch.beatsaber");
+            harmony.PatchAll();
+
+        }
+        /// <summary>
+        /// Called when the script is being destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            Plugin.Log?.Debug($"{name}: OnDestroy()");
+            if (Instance == this)
+                Instance = null; // This MonoBehaviour is being destroyed, so set the static instance property to null.
+
+        }
+        #endregion
+
+        #region Lighting effects
+
+        [HarmonyPatch(typeof(TrackLaneRingsRotationEffect), "SpawnRingRotationEffect", new Type[] { })]
+        public class bhaptics_RingRotationEffect
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                if (ringEffectOff) return;
+                tactsuitVr.PlaySpecialEffect("RingRotation");
+            }
+        }
+
+        [HarmonyPatch(typeof(EnvironmentSpawnRotation), "BeatmapEventAtNoteSpawnCallback", new Type[] { typeof(BeatmapEventData) })]
+        public class bhaptics_LightChangeEffect
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BeatmapEventData beatmapEventData)
+            {
+                // If it's a "special" effect, just play a pattern
+                if ((beatmapEventData.type == BeatmapEventType.Special0) | (beatmapEventData.type == BeatmapEventType.Special1) | (beatmapEventData.type == BeatmapEventType.Special2) | (beatmapEventData.type == BeatmapEventType.Special3))
+                {
+                    tactsuitVr.PlaySpecialEffect(tactsuitVr.myEffectStrings[rnd.Next(myEffectStrings.Count())]);
+                    return;
+                }
+
+                // If last effects has been a while, reduce threshold
+                if (!timerLastEffect.IsRunning) timerLastEffect.Start();
+                if (timerLastEffect.ElapsedMilliseconds >= 2000)
+                {
+                    if (currentTriggerNumber > 1) currentTriggerNumber -= 1;
+                    timerLastEffect.Restart();
+                }
+
+                // Count number of effects at the "same time"
+                if (timerSameTime.ElapsedMilliseconds <= 100)
+                {
+                    numberOfEvents += 1;
+                    timerSameTime.Restart();
+                }
+                else
+                {
+                    numberOfEvents = 0;
+                    timerSameTime.Restart();
+                }
+
+                // If number of simultaneous events is above threshold, trigger effect
+                if (numberOfEvents >= currentTriggerNumber)
+                {
+                    // reset trigger (if it was lowered)
+                    currentTriggerNumber = defaultTriggerNumber;
+                    string effectName = myEffectStrings[rnd.Next(myEffectStrings.Count())];
+                    tactsuitVr.PlaySpecialEffect(effectName);
+
+                    // check if default trigger was set way too high or too low
+                    float weight = (float)numberOfEvents / (float)defaultTriggerNumber / weightFactor;
+                    if (weight > 5.0f) highWeights.Add(weight);
+                    if (weight < 0.24f) highWeights.Add(weight);
+                    // if this happened 4 times in a row, adjust trigger (only down)
+                    if (highWeights.Count >= 4)
+                    {
+                        weightFactor = highWeights.Average();
+                        if (weightFactor < 1.0f)
+                        {
+                            if ((!reducedWeight) && (defaultTriggerNumber > 2))
+                            {
+                                defaultTriggerNumber -= 1;
+                                tactsuitVr.LOG("Trigger adjusted! " + defaultTriggerNumber.ToString() + " " + weightFactor.ToString());
+                            }
+                        }
+                        else reducedWeight = true;
+                        highWeights.Clear();
+                    }
+                }
+
+            }
+        }
+
+        #endregion
+
+        #region Map analysis
+
+        public static void resetGlobalParameters()
+        {
+            highWeights.Clear();
+            weightFactor = 1.0f;
+            reducedWeight = false;
+            defaultTriggerNumber = 4;
+            currentTriggerNumber = 4;
+            ringEffectOff = false;
+        }
+
+        public static void analyzeMap(BeatmapData beatmapData)
+        {
+            // if there are too many ring effects, it gets annoying
+            ringEffectOff = (beatmapData.spawnRotationEventsCount > 50);
+            // count total number of events, estimate trigger number
+            numberOfEvents = beatmapData.beatmapEventsData.Count();
+            defaultTriggerNumber = numberOfEvents / 500;
+            if (defaultTriggerNumber <= 1) defaultTriggerNumber = 2;
+            currentTriggerNumber = defaultTriggerNumber;
+        }
+
+        [HarmonyPatch(typeof(BeatmapDataLoader), "GetBeatmapDataFromBinary", new Type[] { typeof(byte[]), typeof(float), typeof(float), typeof(float) })]
+        public class bhaptics_GetBinaryData
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BeatmapData __result)
+            {
+                analyzeMap(__result);
+            }
+        }
+
+        [HarmonyPatch(typeof(BeatmapDataLoader), "GetBeatmapDataFromJson", new Type[] { typeof(string), typeof(float), typeof(float), typeof(float) })]
+        public class bhaptics_GetJsonData
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BeatmapData __result)
+            {
+                analyzeMap(__result);
+            }
+        }
+
+        [HarmonyPatch(typeof(BeatmapDataLoader), "GetBeatmapDataFromBeatmapSaveData", new Type[] { typeof(List<BeatmapSaveData.NoteData>), typeof(List<BeatmapSaveData.WaypointData>), typeof(List<BeatmapSaveData.ObstacleData>), typeof(List<BeatmapSaveData.EventData>), typeof(BeatmapSaveData.SpecialEventKeywordFiltersData), typeof(float), typeof(float), typeof(float) })]
+        public class bhaptics_GetMemoryData
+        {
+            [HarmonyPostfix]
+            public static void Postfix(BeatmapData __result)
+            {
+                analyzeMap(__result);
+            }
+        }
+
+
+        #endregion
+
+    }
+}
